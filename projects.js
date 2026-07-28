a0317812ce710399806da7a65c5db85566a97e27
Ycreat0r <zavarzin.vlad01@gmail.com>
2026-07-28 14:34:09 +0300

    Add projects.js and update index

---
+++ projects.js
@@ -0,0 +1,14 @@
+const projects = [
+    {
+        title: "Репортаж Lazertag",
+        video: "assets/videos/lazertag.mp4"
+    },
+    {
+        title: "Форум МАИФ Пулково",
+        video: "assets/videos/maif.mp4"
+    },
+    {
+        title: "Posvyat",
+        video: "assets/videos/hero.mp4"
+    }
+];
