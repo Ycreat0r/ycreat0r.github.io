a5165985a794a3f36e2ff0e5fd34d9c40d5d2531
Ycreat0r <zavarzin.vlad01@gmail.com>
2026-07-28 14:46:42 +0300

    check pic and data.js

--- data.js
+++ data.js
@@ -7,12 +7,12 @@ const works = [
 
     {
         id: 1,
-        title: "Репортаж Lazertag",
+        title: "Lazertag_1",
         type: "video",
         category: "commercial",
         year: "2025",
         thumbnail: "assets/images/lazertag.jpg",          // превью (можно то же видео или картинку)
-        video: "assets/videos/Репортаж_Lazertag_1.mp4",   // ← точное имя файла
+        video: "assets/videos/Lazertag_1.mp4",   // ← точное имя файла
         description: "Репортажная съёмка мероприятия Lazertag.",
         tags: ["Репортаж", "Event", "4K"]
     },
@@ -24,7 +24,7 @@ const works = [
         category: "commercial",
         year: "2025",
         thumbnail: "assets/images/maif.jpg",
-        video: "assets/videos/Форум МАИФ Пулково-2_1.mp4",  // ← точное имя файла
+        video: "assets/videos/MAIFF_Pulkovo.mp4",  // ← точное имя файла
         description: "Съёмка форума МАИФ в Пулково.",
         tags: ["Event", "Corporate", "Video"]
     },
